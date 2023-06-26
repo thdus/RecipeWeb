3be656d8f1790c38bd25d82e7ffe41199f3e85db
thdus <sykim0659@naver.com>
2023-06-26 21:44:54 +0900

    header

---
+++ front/front2/src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+      <div id="nav">
+        <h1><router-link to="/"><img src="../assets/5134814.png" width="130"></router-link></h1> 
+        <p><router-link to="/about">레시피게시판</router-link></p> 
+    
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  header{width:100%; text-align:center; position:relative; height:200px; }
+  header h1{position:absolute; top:0; left:30px;}
+  header p{text-align:right;}
+  </style>
